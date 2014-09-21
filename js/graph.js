d60fc5d0c8913b4825ce4b71bf6bd9f02cc720a5
Ralph Bean <rbean@redhat.com>
2014-09-20 21:58:09 -0400

    First stab at status.fp.o integration.

--- js/graph.js
+++ js/graph.js
@@ -3,10 +3,36 @@ var LIGHT_BLUE = "#4872c7";
 var BLACK = "#03060a";
 
 function init() {
+
+    // First thing to do is to query status.fedoraproject.org.
+    // We'll integrate that data into our presentation.
+    var statuses = null;
+    var url;
+    var re = new RegExp('fedoraproject.org');
+    if (re.test(window.location.href)) {
+        // This is the real URL we want to use normally, but it doesn't work in
+        // development due to cross-origin issues.
+        url = 'https://status.fedoraproject.org/statuses.json';
+    } else {
+        // So, when developing, use this URL instead.  It is just a copy we
+        // keep around for testing -- not the genuine article.
+        url = 'js/statuses.json';
+    }
+    $.ajax(url,
+        {
+            dataType: 'json',
+            async: false,
+            success: function(data) { statuses = data.services; },
+        }
+    );
+    // Done with status.fp.o
+
+    // A utility to use later on
     var name2hash = function(name) {
         return $("<p>" + name + "</p>").text().replace(/\s+/g, '');
     }
 
+    // Now, set up our radial graph visualization
     var rgraph = new $jit.RGraph({
         //Where to append the visualization
         injectInto: 'mainvis',
@@ -24,6 +50,18 @@ function init() {
         onBeforeCompute: function(node){
             var header = "<h4>" + node.name + "</h4>";
             var body = "<p>" + node.data.description + "</p>";
+
+            if (node.data.status_mappings != undefined && statuses != null) {
+                body = body + "<table class='table'>"
+                body = body + "<tr><th></th><th>Current Status</th></tr>";
+                $.each(node.data.status_mappings, function(i, status_mapping) {
+                    var status = statuses[status_mapping];
+                    body = body +
+                        "<tr><td>" + status.name + "</td><td>" + status.message + "</td></tr>";
+                });
+                body = body + "</table>"
+            }
+
             var button = "";
             if ( node.data.url != undefined ) {
                 button += "<a href='" + node.data.url + "' target='_blank' class='btn btn-primary'>";
