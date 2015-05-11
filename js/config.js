783bc4a93af9c13e242c023ee671941987457811
Ralph Bean <rbean@redhat.com>
2015-05-11 15:45:27 -0400

    Remove "debug statement"

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@ var config = {
     ipv6: false,
     /* These affect the look of the page and are rendered with handlebars.js */
     name: "Fedora Apps",
-    logo: "<span class='logo'>Fedora</span> apps LOL"
+    logo: "<span class='logo'>Fedora</span> apps"
 }
